d543ec9d8ffdb1967612683f10dcb6d72da31b48
Donquellso <kamil080801@gmail.com>
2025-04-28 21:42:27 +0200

    Updates

--- script.js
+++ script.js
@@ -453,14 +453,18 @@ function isWinner() {
     }
   //diagonal
   if (
-    (gameBoard[0][0] !== "" &&
-      gameBoard[0][0] === gameBoard[1][1] &&
-      gameBoard[1][1] === gameBoard[2][2]) ||
-    (gameBoard[0][2] !== "" &&
-      gameBoard[0][2] === gameBoard[1][1] &&
-      gameBoard[1][1] === gameBoard[2][0])
+    gameBoard[0][0] !== "" &&
+    gameBoard[0][0] === gameBoard[1][1] &&
+    gameBoard[1][1] === gameBoard[2][2]
   ) {
     return gameBoard[0][0];
+  }
+  if (
+    gameBoard[0][2] !== "" &&
+    gameBoard[0][2] === gameBoard[1][1] &&
+    gameBoard[1][1] === gameBoard[2][0]
+  ) {
+    return gameBoard[0][2];
   }
   if (isBoardFull()) {
     return "tie";
